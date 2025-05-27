52984f7ae5ee136e039ce81bc9c53543b57f1bcf
yana-dotcom <jrledesma@up.edu.ph>
2025-05-27 08:41:28 +0800

    feat: add completed generation logic for report 7

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: 'localhost',
     dialect: 'mariadb',
     logging: console.log,
     pool: {
@@ -27,4 +27,5 @@ sequelize.authenticate()
     console.error('Unable to connect to the database:', err);
   });
 
-module.exports = sequelize; 
+module.exports = sequelize;
+
